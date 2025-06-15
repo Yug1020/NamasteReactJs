b5784b32ce6d26354c538bc6798a444cb04be38b
Yug1020 <yugandhardhore@gmail.com>
2025-06-16 00:14:05 +0530

    added completed Ep2

--- Ep2/ByReact2.js
+++ Ep2/ByReact2.js
@@ -31,24 +31,43 @@
 
 
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const div = React.createElement(
-    "div", {id: "parent"},
-    [
-    React.createElement("div", {id: "child1"},
-        [React.createElement("h1", {}, "i am h1 tag"), React.createElement("h2", {}, "i am h2 tag")]
-    ),
-    React.createElement("div", {id: "child2"},
-        [React.createElement("h1", {}, "i am h1 tag"), React.createElement("h2", {}, "i am h2 tag")]
-    )
-    ]
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const child1 = React.createElement("div", { id: "child1", key: "child1" }, [
+  React.createElement("h1", { key: "h1-1" }, "I am h1 tag"),
+  React.createElement("h2", { key: "h2-1" }, "I am h2 tag")
+]);
+
+const child2 = React.createElement("div", { id: "child2", key: "child2" }, [
+  React.createElement("h1", { key: "h1-2" }, "I am h1 tag"),
+  React.createElement("h2", { key: "h2-2" }, "I am h2 tag")
+]);
+
+const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
 
-console.log(div) // you will see here that heading is an object
 
-root.render(div);
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// const div = React.createElement(
+//   "div", { id: "parent" }, [
+//     React.createElement("div", { id: "child1" }, [
+//       React.createElement("h1", {}, "I am h1 tag"),
+//       React.createElement("h2", {}, "I am h2 tag")
+//     ]),
+//     React.createElement("div", { id: "child2" }, [
+//       React.createElement("h1", {}, "I am h1 tag"),
+//       React.createElement("h2", {}, "I am h2 tag")
+//     ])
+//   ]
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(div);
