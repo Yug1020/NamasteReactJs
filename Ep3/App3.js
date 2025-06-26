e0c4646b3f949e08a0d4b9681d8628bfae0aaf0a
Yug1020 <yugandhardhore@gmail.com>
2025-06-26 18:35:43 +0530

    completed EP 3 till part 4

--- Ep3/App3.js
+++ Ep3/App3.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//React method
 const heading = React.createElement(
     "h1", 
     { id: "heading1" }, 
     "Namaste ReactJs 🚀"
 );
 
+// JSX method
 const jsxheader = (<h1 className="heading2" tabIndex = "1">Namaste ReactJs By JSX🚀</h1>)
+
+// Following two are Functioncomponent
+const Title = () => (<h1>Yugandhar Dhore</h1>)
+
+const Functioncomponent = () => (
+    <div> 
+        <Title/> 
+        <h1>Namanster React using Function Component</h1>
+    </div>
+    )
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxheader);
+console.log(jsxheader);
+
+// root.render(jsxheader);
+root.render(<Functioncomponent/>);
 // root.render(heading);
