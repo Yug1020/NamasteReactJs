b5d8de8e6ea7a4b536a41a3b1e7f2f1efb905ba3
Yug1020 <yugandhardhore@gmail.com>
2025-06-27 02:03:41 +0530

    completed Ep3

--- Ep3/App3.js
+++ Ep3/App3.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React method
-const heading = React.createElement(
+const react_element = React.createElement(
     "h1", 
     { id: "heading1" }, 
     "Namaste ReactJs 🚀"
 );
 
-// JSX method
-const jsxheader = (<h1 className="heading2" tabIndex = "1">Namaste ReactJs By JSX🚀</h1>)
 
 // Following two are Functioncomponent
 const Title = () => (<h1>Yugandhar Dhore</h1>)
@@ -17,9 +15,17 @@ const Title = () => (<h1>Yugandhar Dhore</h1>)
 const Functioncomponent = () => (
     <div> 
         <Title/> 
+        {/* {Title()}  another way to call "Title" */}
+        {/* <Title></Title> another way to call "Title" */}
+        <h1>{200 + 300}</h1>
+        {react_element}
         <h1>Namanster React using Function Component</h1>
     </div>
     )
+
+// JSX method
+const jsxheader = (<h1 className="heading2" tabIndex = "1">Namaste ReactJs By JSX🚀 <Functioncomponent/></h1>)
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -27,4 +33,4 @@ console.log(jsxheader);
 
 // root.render(jsxheader);
 root.render(<Functioncomponent/>);
-// root.render(heading);
+// root.render(react_element);
