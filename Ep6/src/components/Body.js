0d4543590a5e56433be4ad61267b8a42900aa7ca
Yug1020 <yugandhardhore@gmail.com>
2025-07-18 23:24:35 +0530

    fix bug in filter

--- Ep6/src/components/Body.js
+++ Ep6/src/components/Body.js
@@ -7,7 +7,7 @@ const Body = () => {
 
   useEffect(() => {
     fetchData();
-  }, [allRes]);
+  }, []);
 
   const fetchData = async () => {
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.9581934&lng=72.8320729&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
