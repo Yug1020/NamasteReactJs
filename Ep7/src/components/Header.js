db416f6f7eb963d7df9032bfd0c1a4c07e05eb31
Yug1020 <yugandhardhore@gmail.com>
2025-11-26 22:32:09 +0530

    starting with Ep 7

---
+++ Ep7/src/components/Header.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { CND_LOGO } from "./utils/constants";
+
+const Header = () =>{
+    const [loginBtn, setloginBtn] = useState("Login")
+
+    return(
+        <div className="Navbar">
+            <div className="burger-logo">            
+                <li><img alt="burger-logo" width="50px" src={CND_LOGO} /></li>
+            </div>
+            <div>
+                <ul className="Navname">
+                    <li>Home</li>
+                    <li>Shopping</li>
+                    <li>About us</li>
+                    <li>Cart</li>
+                    <li><button onClick={()=>{loginBtn === "Login" ? setloginBtn("Logout"): setloginBtn("Login")}}>{loginBtn}</button></li>
+                </ul>
+            </div>
+        </div>
+
+    )
+}
+
+export default Header;
