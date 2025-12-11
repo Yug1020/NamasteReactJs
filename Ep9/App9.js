7834ab382e2d022a060a0a72d74bcfa9a035517d
Yug1020 <yugandhardhore@gmail.com>
2025-12-11 16:00:54 +0530

    Ep 9 first commit

---
+++ Ep9/App9.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./src/components/Header";
+import Body from "./src/components/Body";
+import About from "./src/components/About";
+import Error from "./src/components/Error";
+import Contact from "./src/components/contact";
+import Resmenu from "./src/components/Resmenu";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"
+
+const App = () => {
+    return(
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+        </div>
+    )
+}
+
+const router = createBrowserRouter([
+    {   
+        path: "/",
+        element: <App></App>,
+        children: [
+            {
+                path: "",
+                element: <Body></Body>
+            },
+            {
+                path: "/about",
+                element: <About></About>,
+            },
+            {
+                path: "/contact",
+                element: <Contact></Contact>
+            },
+            {
+                path:"/restaurant_menu/:res_id",
+                element:<Resmenu></Resmenu>,
+            },
+            {
+                path:"/*",
+                element:<Error></Error>
+            }
+            
+        ],
+        // errorElement:<Error></Error>,
+    },
+])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<RouterProvider router={router}/>);
---
+++ Ep9/src/components/Resmenu.js
@@ -0,0 +1,63 @@
+import { res_menu } from "./utils/mock_data_res_menu";
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+
+
+const Resmenu = () => {
+    // simple way to directly call local data file
+    const { res_id } = useParams();
+    const resInfo = res_menu[ res_id ];
+
+    useEffect(()=>{
+        console.log(resInfo)
+    },[])
+
+    if(!resInfo){
+            return(
+            <div className="menupage_parent_div">
+                <div className="menupage_child_div">
+                    <h1>restaurant is offline</h1>
+                </div>
+            </div>
+            )
+        }
+
+    const { name, rating, location, menuItems } = resInfo
+
+    // const [ resInfo, setResInfo ] = useState(null)
+
+    // useEffect( ()=> {
+    //     fetchdata();
+    //     console.log(reslist)
+    // },[])
+
+    // const fetchdata = async() => {
+    //     reslist = res_menu
+    //     setResInfo(reslist);
+    // }
+
+    return(
+        <div className="menupage_parent_div">
+            <div className="menupage_child_div">
+                <h1>{name}</h1>
+                <h2>{rating}</h2>
+                <h2>{location.locality},</h2>
+                {location.locality !== location.areaName && <h3>{location.areaName}</h3>}
+                <h2>Menu</h2>
+                {
+                    menuItems.map((item) =>
+                        (   <ul key={item.id} className="menupage_foodinfo">
+                                <li >{item.name}</li>
+                                <li >{item.price}</li>
+                            </ul>
+                        )
+                    )
+                }
+                <Link to="/"> <h3> ⬅  back to the home page </h3> </Link>    
+            </div>
+        </div>
+        
+    )
+}
+
+export default Resmenu;
